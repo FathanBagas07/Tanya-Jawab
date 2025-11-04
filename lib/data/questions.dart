255e98c4106d2354b6d3139a58d7817f4099a9ba
FathanBagas07 <fatan250217@gmail.com>
2025-11-04 23:38:04 +0700

    feat: Menambahkan model Pertanyaan dan contoh data pertanyaan

---
+++ lib/data/questions.dart
@@ -0,0 +1,14 @@
+import '../models/question.dart';
+
+final List<Question> questions = [
+  Question(
+    question: 'Apa jurusan yang ada di FASILKOM-TI Universitas Sumatera Utara?',
+    options: ['Ilmu Komputer', 'Matematika', 'Keperawatan', 'Teknik Elektro'],
+    answerIndex: 0,
+  ),
+  Question(
+    question: 'Bahasa pemrograman yang digunakan untuk Flutter?',
+    options: ['Dart', 'Python', 'Java', 'C#'],
+    answerIndex: 0,
+  ),
+];
---
+++ lib/models/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  final String question;
+  final List<String> options;
+  final int answerIndex;
+
+  Question({
+    required this.question,
+    required this.options,
+    required this.answerIndex,
+  });
+}
